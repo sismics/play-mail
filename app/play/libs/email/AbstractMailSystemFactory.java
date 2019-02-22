37679cce151801194d9feec550b9daad5a3327eb
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2019-02-22 16:12:30 +0100

    Revert: MockMailer: allow storing all sent mails instead of only the last one

--- app/play/libs/email/AbstractMailSystemFactory.java
+++
@@ -1,9 +0,0 @@
-package play.libs.email;
-
-public abstract class AbstractMailSystemFactory {
-
-    public static final AbstractMailSystemFactory DEFAULT = new AppMailSystemFactory();
-
-    public abstract MailSystem currentMailSystem();
-
-}
